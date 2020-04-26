b27a7ca28228ed61e4dcab9a59a201fa6902a968
Karan Sheth <karan.sheth@nutanix.com>
2020-04-26 09:51:13 +0530

    adds the ability to map over constants

--- src/utils/keyMirror.js
+++ src/utils/keyMirror.js
@@ -1,13 +1,31 @@
 /**
+ * Same as a regular keyMirror, but it has a map method.
+ * @param {...Strings} keys The constant values
+ * @returns {void}
+ */
+function Constant(...keys) {
+    keys.forEach(d => (this[d] = d));
+}
+
+Constant.prototype.map = function forEach(fn) {
+    let newArray = [],
+        idx = 0;
+    for (let key in this) {
+        if (this.hasOwnProperty(key)) {
+            newArray[idx++] = fn(key);
+        }
+    }
+    return newArray;
+};
+
+/**
  * Key mirror implemetation that creates a constants object. Different signature
- * from npm(keyMirror)
+ * from npm(keyMirror).
  * example: keymirror('CONSTANT1', 'CONSTANT2')
  * @param  {...String} keys n number of strings used to create constants object.
  * @return {Object}         Object where { arg1: arg1, arg2: arg2 }, etc. which
  *                          can be used as constant values.
  */
 export default function keyMirror(...keys) {
-    const o = {};
-    keys.forEach(d => (o[d] = d));
-    return o;
+    return new Constant(...keys);
 }
