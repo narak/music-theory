f382bf6ace0e952db29d5f53e010b59e96670434
Karan Sheth <karansheth@gmail.com>
2025-07-30 16:01:17 -0700

    fix shadcn ai code

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/ui': path.resolve(__dirname, './src/components/ui'),
+      '@/lib': path.resolve(__dirname, './src/lib'),
+      '@/constants': path.resolve(__dirname, './src/constants'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
     },
   },
   server: {
